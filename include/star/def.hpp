037b81138e7fa341248fcd37a7c4ed319505d034
cosmos-in-h2o <dream77070@163.com>
2024-07-20 11:41:32 +0800

    def and star.hpp

---
+++ include/star/def.hpp
@@ -0,0 +1,67 @@
+#ifndef STAR_DEF_HPP
+#define STAR_DEF_HPP
+
+#include <limits>
+
+#ifdef _WIN32
+#ifdef STAR_DLL_EXPORT
+#define STAR_API __declspec(dllexport)
+#else
+#define STAR_API __declspec(dllimport)
+#endif
+#else
+#define STAR_API
+#endif
+
+namespace star {
+using int8 = signed char;
+using int16 = signed short int;
+using int32 = signed int;
+using int64 = signed long long int;
+using uint8 = unsigned char;
+using uint16 = unsigned short int;
+using uint32 = unsigned int;
+using uint64 = unsigned long long int;
+using float32 = float;
+using float64 = double;
+using float128 = long double;
+
+constexpr int8 int8_min = (std::numeric_limits<int8>::min)();
+constexpr int8 int8_max = (std::numeric_limits<int8>::max)();
+constexpr int16 int16_min = (std::numeric_limits<int16>::min)();
+constexpr int16 int16_max = (std::numeric_limits<int16>::max)();
+constexpr int32 int32_min = (std::numeric_limits<int32>::min)();
+constexpr int32 int32_max = (std::numeric_limits<int32>::max)();
+constexpr int64 int64_min = (std::numeric_limits<int64>::min)();
+constexpr int64 int64_max = (std::numeric_limits<int64>::max)();
+constexpr uint8 uint8_min = (std::numeric_limits<uint8>::min)();
+constexpr uint8 uint8_max = (std::numeric_limits<uint8>::max)();
+constexpr uint16 uint16_min = (std::numeric_limits<uint16>::min)();
+constexpr uint16 uint16_max = (std::numeric_limits<uint16>::max)();
+constexpr uint32 uint32_min = (std::numeric_limits<uint32>::min)();
+constexpr uint32 uint32_max = (std::numeric_limits<uint32>::max)();
+constexpr uint64 uint64_min = (std::numeric_limits<uint64>::min)();
+constexpr uint64 uint64_max = (std::numeric_limits<uint64>::max)();
+
+constexpr float32 float32_min = (std::numeric_limits<float32>::min)();
+constexpr float32 float32_max = (std::numeric_limits<float32>::max)();
+constexpr float64 float64_min = (std::numeric_limits<float64>::min)();
+constexpr float64 float64_max = (std::numeric_limits<float64>::max)();
+constexpr float128 float128_min = (std::numeric_limits<float128>::min)();
+constexpr float128 float128_max = (std::numeric_limits<float128>::max)();
+#if __SIZEOF_POINTER__ == 4
+using int_t = int32;
+using uint_t = uint32;
+#else
+using int_t = int64;
+using uint_t = uint64;
+#endif
+} // namespace star
+
+#ifdef STAR_ENABLE_ASSERTS
+#define STAR_ASSERT(x,...){if(!x){star::Log::engineError("Assertion failed:{0}",__VA_ARGS__);__debugbreak();}}
+#else
+#define STAR_ASSERT(x,...)
+#endif
+
+#endif
---
+++ include/star/star.hpp
@@ -0,0 +1,18 @@
+#include <star/def.hpp>
+
+//core
+#include <star/core/io/log.hpp>
+#include <star/core/application.hpp>
+#include <star/core/window.hpp>
+#include <star/core/event/event.hpp>
+//#include <star/core/>
+
+//platform
+#include <star/platform/windows/windows_window.hpp>
+
+//rtl
+#include <star/rtl/ref.hpp>
+#include <star/rtl/string_view.hpp>
+#include <star/rtl/string.hpp>
+#include <star/rtl/unique_ptr.hpp>
+#include <star/rtl/weak_ptr.hpp>
